cb14f8c54e6dc96bcff6a5923e12c8074a44481c
duy <duyducnguyen107@gmail.com>
2024-05-09 11:26:02 +0700

    first commit

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,13 +22,14 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
-
+    gazebo_params_path = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml') 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items() 
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -36,7 +37,7 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
-<<<<<<< HEAD
+
     
     diff_drive_spawner = Node(
     package="controller_manager",
@@ -48,19 +49,11 @@ def generate_launch_description():
     executable="spawner",
     arguments=["joint_broad"],
     )
-=======
-
-
->>>>>>> e2c564ac18a9e73b7e968ace8e992ae9022791a9
-
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
-<<<<<<< HEAD
         diff_drive_spawner,
         joint_broad_spawner
-=======
->>>>>>> e2c564ac18a9e73b7e968ace8e992ae9022791a9
     ])
